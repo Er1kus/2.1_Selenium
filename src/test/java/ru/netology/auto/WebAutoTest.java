b7ffc690fa64d368401fe70fbd4d6c9e540a4662
Er1kus <goryaev86@mail.ru>
2022-08-01 17:25:42 +0300

    driver relocated in directory win

---
+++ src/test/java/ru/netology/auto/WebAutoTest.java
@@ -0,0 +1,11 @@
+package ru.netology.auto;
+import org.junit.jupiter.api.BeforeAll;
+
+public class WebAutoTest {
+
+    @BeforeAll
+    public void setUp(){
+//        System.setProperty("webdriver.chrome.driver","C:\JavaNet\Selenium\driver");
+    }
+
+}
